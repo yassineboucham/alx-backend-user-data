2f5c97cf623c56b683c07567be6e26f86134a37d
yassineboucham <yassineboucham2002@gmail.com>
2025-01-16 11:56:20 +0100

    all

--- 0x00-personal_data/README.md
+++ 0x00-personal_data/README.md
@@ -1,1 +1,1 @@
-0x00-personal_data
+# Personal Data
--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+"""A module for encrypting passwords.
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
+    """Hashes a password using a random salt.
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
+    """Checks is a hashed password was formed from the given password.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
